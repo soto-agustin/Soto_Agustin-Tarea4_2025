1f35b41e269cd3ca3abbc70629cd81f757d829c6
soto-agustin <secundariasoto@gmail.com>
2025-10-03 20:07:18 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import {resta, sumar } from './math.js' 
 createRoot(document.getElementById('root')).render(
   <App />
 )
